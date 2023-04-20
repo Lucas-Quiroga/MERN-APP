a24ab33d3f404c31706f8be8136a8c4dd619f39a
Lucas-Quiroga <quiroga.lucasoffice@gmail.com>
2023-04-20 20:35:03 -0300

    endpoint GET con node y express

--- controllers/productController.js
+++ controllers/productController.js
@@ -1,6 +1,6 @@
 const Product = require("../models/Product");
 
-//fúncion para crear producto
+//fúncion para crear producto (MÉTODO POST)
 async function addProduct(req, res) {
   try {
     const { name, size, unitaryPrice, description } = req.body;
@@ -25,6 +25,14 @@ async function addProduct(req, res) {
   }
 }
 
+//fúncion para obtener producto (MÉTODO GET)
+async function getProducts(req, res) {
+  //lean = lo que retorne la consulta lo devuelve en objetos planos de js | exec = indica que ejecuta la consulta y retorna la promesa (todos son metodos de mongoose)
+  const products = await Product.find().lean().exec();
+  res.status(200).send({ products });
+}
+
 module.exports = {
   addProduct,
+  getProducts,
 };
